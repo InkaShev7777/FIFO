ee407b46fded1ce466edde4610a3df7541345084
Ilya Shevchenko <illyashev2905@gmail.com>
2022-01-12 15:24:28 +0200

    end

--- FIFO/FIFO/FIFO.cpp
+++ FIFO/FIFO/FIFO.cpp
@@ -1,8 +1,138 @@
 #include <iostream>
+using namespace std;
+template<typename T>
+struct Node
+{
+    T value;
+    Node* next;
+    Node()
+    {
 
+    }
+    Node(Node* node)
+    {
+        next = nullptr;
+        value = node->value;
+    }
+};
+template<typename T>
+class List {
+    Node<T>* head;
+    int size;
+public:
+    List()
+    {
+        this->head = nullptr;
+        this->size = 0;
+    }
+    int getSize()
+    {
+        return this->size;
+    }
+    void Add(T value)
+    {
+        if (this->head == nullptr)
+        {
+            this->head = new Node<T>();
+            this->head->value = value;
+            this->head->next = nullptr;
+        }
+        else
+        {
+            Node<T>* tmp = this->head;
+            while (tmp->next != nullptr)
+            {
+                tmp = tmp->next;
+            }
+            tmp->next = new Node<T>();
+            tmp->next->value = value;
+            tmp->next->next = nullptr;
+        }
+        size++;
+    }
+
+    void print() {
+        Node<T>* iter = this->head;
+        
+            while (iter != nullptr)
+            {
+                cout << "Adres: " << iter << "\tValue: " << iter->value << "\tNext: " << iter->next << "\n";
+                iter = iter->next;
+            }
+         delete iter;
+    }
+
+    Node<T>* dell()
+    {
+        Node<T>* iter = this->head;
+        if (this->size > 0)
+        {
+            if (iter != nullptr)
+            {
+                Node<T>* ret = new Node<T>(iter);
+                // delete iter;
+                iter = iter->next;
+                this->head = iter;
+                size--;
+                return ret;
+            }
+        }
+        else
+        {
+            size--;
+        }
+    }
+};
 int main()
 {
-    std::cout << "Hello World!\n";
+    List<int> list;
+    setlocale(0, "");
+    int vibor;
+    Node<int>* ret;
+    do
+    {
+        cout << "\t\tМеню\n";
+        cout << "1 - Добавление элемента\n";
+        cout << "2 - Вывод списка\n";
+        cout << "3 - Удаление элементов логикой FIFO\n";
+        cout << "0 - Выход\n";
+        cout << "Сделайте свой выбор: ";
+        cin >> vibor;
+
+        switch (vibor)
+        {
+        case 1:
+            system("cls");
+            int a;
+            cout << "Введите элемент который хотите добавить: ";
+            cin >> a;
+            list.Add(a);
+            system("pause");
+            system("cls");
+            break;
+        case 2:
+            system("cls");
+            list.print();
+            system("pause");
+            system("cls");
+            break;
+        case 3:
+            system("cls");
+            ret = list.dell();
+            if (list.getSize() >= 0)
+            {
+                cout<< "Новый адрес: " << ret << "\tЗначение: " << ret->value<<"\tNext: "<<ret->next<< "\n";
+            }
+            else
+            {
+                cout << "Список пуст!!!\n";
+            }
+            system("pause");
+            system("cls");
+            break;
+
+        }
+    } while (vibor != 0);
 }
 
 
---
+++ LIFO/LIFO/LIFO.cpp
@@ -0,0 +1,89 @@
+// ConsoleApplication3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <iostream>
+template<typename T>
+struct Node {
+	T value;
+	Node* next;
+
+	Node()
+	{
+
+	}
+
+	Node(Node* node) {
+		next = NULL;
+		value = node->value;
+	}
+};
+
+
+template <typename T>
+class LIFO {
+private:
+	Node<T>* first;
+	unsigned long size;
+public:
+	explicit LIFO()
+	{
+		this->first = NULL;
+		this->size = 0;
+	}
+
+	void add(T value) {
+		if (this->first == NULL) {
+			this->first = new Node<T>();
+			this->first->value = value;
+			this->first->next = NULL;
+		}
+		else
+		{
+			Node<T>* iter = this->first;
+			while (iter->next != NULL)
+			{
+				iter = iter->next;
+			}
+
+			iter->next = new Node<T>();
+			iter->next->value = value;
+			iter->next->next = NULL;
+		}
+
+	}
+	void show() {
+		Node<T>* iter = this->first;
+		while (iter != NULL)
+		{
+			std::cout << iter << " " << iter->next << " " << iter->value << "\n";
+			iter = iter->next;
+		}
+
+	}
+
+	Node<T>* remove() {
+		Node<T>* iter = this->first;
+		while (iter->next != NULL)
+		{
+			iter = iter->next;
+		}
+		Node<T>* forReturn = new Node<T>(iter);
+		delete iter;
+		return forReturn;
+	}
+};
+int main()
+{
+	LIFO<int> simple;
+
+	simple.add(0);
+	simple.add(1);
+	simple.add(2);
+
+	simple.show();
+
+	Node<int>* last = simple.remove();
+	std::cout << last << " " << last->value << "\n";
+	//simple.show();
+	Node<int>* last1 = simple.remove();
+	std::cout << last1 << " " << last1->value << "\n";
+}
